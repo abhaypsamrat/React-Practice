83f785e26e613b84f4a4f8a91f3e77e2631f8858
Abhay Pratap Samrat <60846174+abhaypsamrat@users.noreply.github.com>
2023-01-15 19:17:44 +0530

    props added

---
+++ props in react/src/App.js
@@ -0,0 +1,12 @@
+import Welcome from "./Welcome";
+
+function App() {
+  return (
+    <div>
+      <Welcome name="Jhon" city="Delhi"/>
+      <Welcome name="Abhi" city="Mumbai"/>
+      <Welcome name="Vishash" city="Varanasi"/>
+    </div>
+  );
+}
+export default App;
---
+++ props in react/src/Welcome.js
@@ -0,0 +1,11 @@
+function Welcome(props) {
+  return (
+    <div>
+      <div>Hello! Mr. X, I'm {props.name}</div>
+      <div>I am from {props.city}</div>
+      <div>I came here to welcome you</div>
+    </div>
+  );
+}
+
+export default Welcome;
